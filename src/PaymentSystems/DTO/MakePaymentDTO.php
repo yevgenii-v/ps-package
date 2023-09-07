d4ae7340c2eecdcd02df3525c9f620b411456819
Yevgenii <sag3labs@gmail.com>
2023-09-07 18:17:54 +0300

    Fix orderId in liqpay invoice

--- src/PaymentSystems/DTO/MakePaymentDTO.php
+++ src/PaymentSystems/DTO/MakePaymentDTO.php
@@ -9,11 +9,13 @@ class MakePaymentDTO
     /**
      * @param float $amount
      * @param Currency $currency
+     * @param string $orderId
      * @param string $description
      */
     public function __construct(
         protected float $amount,
         protected Currency $currency,
+        protected string $orderId,
         protected string $description = '',
     ) {
     }
@@ -32,6 +34,14 @@ class MakePaymentDTO
     public function getCurrency(): Currency
     {
         return $this->currency;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrderId(): string
+    {
+        return $this->orderId;
     }
 
     /**
